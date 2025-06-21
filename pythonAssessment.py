59fe49daa526ff609d6473771d7ede0008c79b65
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-06-21 07:04:45 -0600

    updated functions to handle empty string and changed functions to return value instead of print

--- pythonAssessment.py
+++ pythonAssessment.py
@@ -1,73 +1,61 @@
-from text_analysis_manager.validation import validate_task_word
+import re
 from collections import Counter
 
 # Read article in to string variable      
-def string_article(file_path):
+def load_article(file_path):
     with open(file_path, 'r') as file:
         return file.read()
      
 #Count Specific Word
-def identify_most_common_word(text, word):
-    try:
-        validate_task_word(word)
-
-        if not text.strip():
-            print(f"The word '{word}' occurs 0 time(s) in the article.")
-            return 0
-
-        words = text.lower().split()
-        word_count = Counter(words)
-        if word.lower() in words:
-            number = word_count[word.lower()]
-            print(f"The word '{word}' occurs {number} time(s) in the article.")
-            return number
-        else:
-            print(f"The word '{word}' does not occur in the article.")
-            return 0
-    except ValueError as e:
-        print(e)
-        return 0
+def count_specific_word(text, word):
+    if not text.strip():
+          return 0
+    words = re.findall(r'\b\w+\b', text.lower())
+    count = words.count(word.lower())
+    return count
 
 #Identify Most Common Word
-def most_common_word(text):
+def identify_most_common_word(text):
 	words = text.split()
 	word_count = Counter(words)
 	most_common = max(word_count, key=word_count.get)
-	print(most_common)
+	return most_common
 
 #Calculate Average Word Length
 def calculate_average_word_length(text):
-	words = text.split()
-	total_length = sum(len(word) for word in words)
-	num_words = len(words)
+    words = text.split()
+    if not words:
+        return 0
 
-	if num_words > 0:
-		average_length = total_length / num_words
-		print(f"The average word length is: {average_length:.2f}")
-	else:
-		print("No words in article")
-	
+    total_length = sum(len(word.strip(".,!?;:")) for word in words)
+    average_lenth = total_length / len(words)
+    return round(average_lenth , 2)
+
 #Count Number of Paragraphs
 def count_paragraphs(text):
-	paragraphs = [p for p in text.split('\n\n') if p.strip()]
-	paragraph_count = len(paragraphs)
-	print(f"Number of paragraphs: {paragraph_count}")
+    if not text.strip():
+        return 1
+    paragraphs = [p for p in text.split('\n\n') if p.strip()]
+    paragraph_count = len(paragraphs)
+    return paragraph_count
 
 #Count Number of Sentences
 def count_sentences(text):
+    if not text.strip():
+        return 1
     sentence_endings = {".", "!", "?"}
     count = 0 
     for index in range(len(text)):
         if text[index] in sentence_endings:
              if index == len(text) - 1 or text[index + 1] == " ":
                   count += 1
-    print(f"Number of sentences in article: {count}")    	
+    return count  	
 
 # Main Text Analysis Function
 def main():
     file_path = 'article.txt'
-    text = string_article(file_path)
-
+    text = load_article(file_path)
+    
     while True:
         print("Text Analysis System")
         print("1. Count Specific Word Occurance")
@@ -80,15 +68,20 @@ def main():
 
         if choice == "1":
             word = input("What would would you like to search the occurance of: ")
-            identify_most_common_word(text, word)             
-        elif choice == "2":     
-            most_common_word(text)
+            count = count_specific_word(text, word)
+            print(f"{count}")            
+        elif choice == "2":
+            word = identify_most_common_word(text)
+            print(f"{word}")
         elif choice == "3":
-            calculate_average_word_length(text)
+            word_length = calculate_average_word_length(text)
+            print(f"{word_length}")
         elif choice == "4":
-            count_paragraphs(text)
+            number_of_p = count_paragraphs(text)
+            print(f"{number_of_p}")
         elif choice == "5":
-            count_sentences(text)
+            number_of_s = count_sentences(text)
+            print(f"{number_of_s}")
         elif choice == "6":
             print("Exiting the program...")
             break
--- text_analysis_manager/validation.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def validate_task_word(word):
-    if not re.match(r'^[A-Za-z]+$', word):
-        raise ValueError("word can only contain letters.")
-    if len(word) > 10:
-        raise ValueError("Word cannot be more than 10 characters.")
